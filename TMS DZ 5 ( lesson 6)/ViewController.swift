ec8df6e6b9ece5b44eebacefd8d9f2bd2404d524
Egor Kartshev <egorkarta982@gmail.com>
2022-06-10 18:48:11 +0300

    Initial Commit

---
+++ TMS DZ 5 ( lesson 6)/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  TMS DZ 5 ( lesson 6)
+//
+//  Created by Егор on 10.06.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static var random: UIColor {
+        return UIColor(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1),
+            alpha: 1.0
+        )
+    }
+}
+
+class ViewController: UIViewController {
+
+    let circleButton = UIButton()
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        circleButton.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        circleButton.layer.cornerRadius = circleButton.frame.size.width / 2
+        circleButton.backgroundColor = .red
+        circleButton.addTarget(self, action: #selector (tapOnCircleButton), for: .touchUpInside)
+        self.view.addSubview(circleButton)
+    }
+    
+@objc    func tapOnCircleButton(){
+    let screenWidght = Int(view.frame.size.width) - Int(circleButton.frame.size.width)
+    let screenHeight = Int(view.frame.size.height) - Int(circleButton.frame.size.height)
+    
+    circleButton.frame = CGRect(x: Int.random(in: 0...screenWidght), y: Int.random(in: 0...screenHeight), width: 100, height: 100)
+    circleButton.layer.cornerRadius = circleButton.frame.size.width / 2
+    circleButton.backgroundColor = UIColor.random
+    
+    }
+    
+    
+    
+    
+}
+
