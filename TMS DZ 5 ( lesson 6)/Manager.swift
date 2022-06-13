8b09acee16b6ddaa112cf75a95fd3ec3881f3358
Egor Kartshev <egorkarta982@gmail.com>
2022-06-13 16:20:13 +0300

    second ( squares) Done

---
+++ TMS DZ 5 ( lesson 6)/Manager.swift
@@ -0,0 +1,16 @@
+//
+//  Manager.swift
+//  TMS DZ 5 ( lesson 6)
+//
+//  Created by Егор on 12.06.2022.
+//
+
+import Foundation
+
+class Manager {
+    static var shared = Manager()
+    
+    private init(){}
+    
+        
+}
--- TMS DZ 5 ( lesson 6)/ViewController.swift
+++ TMS DZ 5 ( lesson 6)/ViewController.swift
@@ -21,31 +21,79 @@ extension UIColor {
 class ViewController: UIViewController {
 
     let circleButton = UIButton()
-   
+    let squaresButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // создаем круг
         
         circleButton.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
         circleButton.layer.cornerRadius = circleButton.frame.size.width / 2
         circleButton.backgroundColor = .red
         circleButton.addTarget(self, action: #selector (tapOnCircleButton), for: .touchUpInside)
         self.view.addSubview(circleButton)
+        
+        // создаем кнопку
+        
+        squaresButton.addTarget(self, action: #selector(squaresButtonPressed), for: .touchUpInside)
+        squaresButton.frame = CGRect(x: view.frame.width / 2 - 50, y: view.frame.height - 60, width: 100, height: 30)
+        squaresButton.backgroundColor = .blue
+        squaresButton.setTitle("button", for: .normal)
+        self.view.addSubview(squaresButton)
+        
     }
     
-@objc    func tapOnCircleButton(){
-    let screenWidght = Int(view.frame.size.width) - Int(circleButton.frame.size.width)
-    let screenHeight = Int(view.frame.size.height) - Int(circleButton.frame.size.height)
     
-    circleButton.frame = CGRect(x: Int.random(in: 0...screenWidght), y: Int.random(in: 0...screenHeight), width: 100, height: 100)
-    circleButton.layer.cornerRadius = circleButton.frame.size.width / 2
-    circleButton.backgroundColor = UIColor.random
     
+    
+    @objc   func tapOnCircleButton(){
+        let screenWidght = Int(view.frame.size.width) - Int(circleButton.frame.size.width)
+        let screenHeight = Int(view.frame.size.height) - Int(circleButton.frame.size.height)
+        
+        circleButton.frame = CGRect(x: Int.random(in: 0...screenWidght), y: Int.random(in: 0...screenHeight), width: 100, height: 100)
+        circleButton.layer.cornerRadius = circleButton.frame.size.width / 2
+        circleButton.backgroundColor = UIColor.random
+        
     }
     
-    // ghbny
+    // функция для заполнения квадратами
     
-    // закомитить на гитхаб
+    @objc func squaresButtonPressed() {
+        
+        self.circleButton.removeFromSuperview()
+        
+        var originX = 0
+        var originY = 0
+        let width = 100
+        let height = 100
+        
+        while originY < Int(self.view.frame.height - CGFloat(height)){
+            while originX < Int(self.view.frame.width - CGFloat(width)){
+                createSquare(x: originX, y: originY, width: width, height: height)
+                originX += width
+            }
+            originX = 0
+            originY += height
+        }
+    }
     
-}
+    
+    func createSquare (x: Int, y: Int, width: Int, height: Int){
+        let newSqaure = UIView()
+        newSqaure.frame = CGRect(x: x, y: y, width: width, height: height)
+        newSqaure.backgroundColor = .random
+        self.view.addSubview(newSqaure)
+    }
+    
+
+    
+    
+        
+    }
+    
+   
+    
+    
+    
 
